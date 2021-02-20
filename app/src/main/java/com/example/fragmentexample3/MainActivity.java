29c6326b85fbcb877d5be0a6a9bc344a4bd619fd
czhang83 <cynthiaz.cz34@gmail.com>
2021-02-20 20:29:24 +0800

    Add third fragment.

--- app/src/main/java/com/example/fragmentexample3/MainActivity.java
+++ app/src/main/java/com/example/fragmentexample3/MainActivity.java
@@ -14,10 +14,34 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private boolean threePane = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //if cannot find the second fragment in the layout
+        // meas only 1 column - portrait
+        // else landscape
+        if (findViewById(R.id.fragmentContainerView_land_second) != null){
+            threePane = true;
+        }
+
+        // loading in whether 1 or 2 fragments based on this boolean
+        if (!threePane){
+            // portrait
+            loadFragment(new FirstFragment(), R.id.fragmentContainer_first);
+            Button button_personality = findViewById(R.id.button_personality);
+            button_personality.setOnClickListener(v -> launchSecondActivity(v));
+
+            Button button_house_info = findViewById(R.id.button_house_info);
+            button_house_info.setOnClickListener(v -> launchThirdActivity(v));
+        }
+        else {
+            loadFragment(new FirstFragment(), R.id.fragmentContainer_land_first);
+            loadFragment(new SecondFragment(), R.id.fragmentContainerView_land_second);
+            loadFragment(new ThirdFragment(), R.id.fragmentContainerView_land_third);
+        }
 
     }
 
@@ -30,8 +54,13 @@ public class MainActivity extends AppCompatActivity {
         fragmentTransaction.commit();
     }
 
-    public void launchActivity(View view){
+    public void launchSecondActivity(View view){
         Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
+    }
+
+    public void launchThirdActivity(View view){
+        Intent intent = new Intent(this, ThirdActivity.class);
         startActivity(intent);
     }
 }
